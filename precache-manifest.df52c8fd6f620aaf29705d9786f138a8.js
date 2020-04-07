91dde4cdffa040f5c2ae5bd5cebd08f68b900caa
Khadija <khadija.alselini@outlook.com>
2020-04-07 22:22:46 +0100

    Updates

---
+++ precache-manifest.df52c8fd6f620aaf29705d9786f138a8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ff2e942410eb0ff4feba9537ce0b6b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "822c959e6226bd9f50a7",
+    "url": "/static/css/main.0b1d8aa0.chunk.css"
+  },
+  {
+    "revision": "c537b2098a11374642c0",
+    "url": "/static/js/2.2352bb35.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2352bb35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "822c959e6226bd9f50a7",
+    "url": "/static/js/main.4551f3c6.chunk.js"
+  },
+  {
+    "revision": "fdceeae38310c6ea805d",
+    "url": "/static/js/runtime-main.bc8a832e.js"
+  },
+  {
+    "revision": "82db9ce0b6f2a17565737a6b5eba5a25",
+    "url": "/static/media/Picture_1.82db9ce0.jpg"
+  },
+  {
+    "revision": "f30790c5e024399cc48ec3ab147e0dba",
+    "url": "/static/media/Thank-U-sign.f30790c5.jpg"
+  },
+  {
+    "revision": "77e43999086d8b838984b1dd2b2659e4",
+    "url": "/static/media/heat_map_europe.77e43999.jpg"
+  },
+  {
+    "revision": "cda77cbf59a48c774816849f6d318f9c",
+    "url": "/static/media/logo.cda77cbf.png"
+  },
+  {
+    "revision": "ddd51dc8b8517d5ca8edf95b714b053b",
+    "url": "/static/media/new_york_city.ddd51dc8.jpg"
+  },
+  {
+    "revision": "09ce7f3cf404de55c41c6c9f0775dee4",
+    "url": "/static/media/woman-smartphone.09ce7f3c.jpg"
+  }
+]);
